24573ead85fedd71bd1c705c9e2fa75192644a2e
AlexisMelvin <tgkol.hhop33@gmail.com>
2024-05-30 21:42:13 -0600

    Se crearon las vistas de gestion, paises, departamentos, municipios, distritos

---
+++ SIGIV/GUI/Departamentos/GestionDepartamentos.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SIGIV.GUI.Departamentos
+{
+    partial class GestionDepartamentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.rbEliminar = new System.Windows.Forms.RadioButton();
+            this.rbModificar = new System.Windows.Forms.RadioButton();
+            this.rbNuevo = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.tmbPaises = new System.Windows.Forms.ComboBox();
+            this.dtgDatos = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(15);
+            this.panel1.Size = new System.Drawing.Size(441, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dtgDatos);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(15, 184);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(411, 251);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lista de Paises";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEjecutar);
+            this.groupBox2.Controls.Add(this.rbEliminar);
+            this.groupBox2.Controls.Add(this.rbModificar);
+            this.groupBox2.Controls.Add(this.rbNuevo);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(15, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(411, 75);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operaciones";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(316, 34);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 3;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            // 
+            // rbEliminar
+            // 
+            this.rbEliminar.AutoSize = true;
+            this.rbEliminar.Location = new System.Drawing.Point(197, 37);
+            this.rbEliminar.Name = "rbEliminar";
+            this.rbEliminar.Size = new System.Drawing.Size(61, 17);
+            this.rbEliminar.TabIndex = 2;
+            this.rbEliminar.TabStop = true;
+            this.rbEliminar.Text = "Eliminar";
+            this.rbEliminar.UseVisualStyleBackColor = true;
+            // 
+            // rbModificar
+            // 
+            this.rbModificar.AutoSize = true;
+            this.rbModificar.Location = new System.Drawing.Point(98, 37);
+            this.rbModificar.Name = "rbModificar";
+            this.rbModificar.Size = new System.Drawing.Size(68, 17);
+            this.rbModificar.TabIndex = 1;
+            this.rbModificar.TabStop = true;
+            this.rbModificar.Text = "Modificar";
+            this.rbModificar.UseVisualStyleBackColor = true;
+            // 
+            // rbNuevo
+            // 
+            this.rbNuevo.AutoSize = true;
+            this.rbNuevo.Location = new System.Drawing.Point(18, 37);
+            this.rbNuevo.Name = "rbNuevo";
+            this.rbNuevo.Size = new System.Drawing.Size(57, 17);
+            this.rbNuevo.TabIndex = 0;
+            this.rbNuevo.TabStop = true;
+            this.rbNuevo.Text = "Nuevo";
+            this.rbNuevo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tmbPaises);
+            this.groupBox1.Controls.Add(this.txbNombre);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(15, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(411, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formulario";
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(18, 57);
+            this.txbNombre.Multiline = true;
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(373, 20);
+            this.txbNombre.TabIndex = 1;
+            // 
+            // tmbPaises
+            // 
+            this.tmbPaises.FormattingEnabled = true;
+            this.tmbPaises.Location = new System.Drawing.Point(18, 20);
+            this.tmbPaises.Name = "tmbPaises";
+            this.tmbPaises.Size = new System.Drawing.Size(373, 21);
+            this.tmbPaises.TabIndex = 2;
+            // 
+            // dtgDatos
+            // 
+            this.dtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgDatos.Location = new System.Drawing.Point(3, 16);
+            this.dtgDatos.Name = "dtgDatos";
+            this.dtgDatos.Size = new System.Drawing.Size(405, 232);
+            this.dtgDatos.TabIndex = 0;
+            // 
+            // GestionDepartamentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "GestionDepartamentos";
+            this.Text = "GestionDepartamentos";
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.RadioButton rbEliminar;
+        private System.Windows.Forms.RadioButton rbModificar;
+        private System.Windows.Forms.RadioButton rbNuevo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.ComboBox tmbPaises;
+        private System.Windows.Forms.DataGridView dtgDatos;
+    }
+}
---
+++ SIGIV/GUI/Paises/GestionPaises.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SIGIV.GUI.Paises
+{
+    partial class GestionPaises
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbNuevo = new System.Windows.Forms.RadioButton();
+            this.rbModificar = new System.Windows.Forms.RadioButton();
+            this.rbEliminar = new System.Windows.Forms.RadioButton();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(15);
+            this.panel1.Size = new System.Drawing.Size(440, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txbNombre);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(15, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 69);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formulario";
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(18, 29);
+            this.txbNombre.Multiline = true;
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(373, 20);
+            this.txbNombre.TabIndex = 1;
+            this.txbNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEjecutar);
+            this.groupBox2.Controls.Add(this.rbEliminar);
+            this.groupBox2.Controls.Add(this.rbModificar);
+            this.groupBox2.Controls.Add(this.rbNuevo);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(15, 84);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(410, 83);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operaciones";
+            // 
+            // rbNuevo
+            // 
+            this.rbNuevo.AutoSize = true;
+            this.rbNuevo.Location = new System.Drawing.Point(18, 37);
+            this.rbNuevo.Name = "rbNuevo";
+            this.rbNuevo.Size = new System.Drawing.Size(57, 17);
+            this.rbNuevo.TabIndex = 0;
+            this.rbNuevo.TabStop = true;
+            this.rbNuevo.Text = "Nuevo";
+            this.rbNuevo.UseVisualStyleBackColor = true;
+            this.rbNuevo.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbModificar
+            // 
+            this.rbModificar.AutoSize = true;
+            this.rbModificar.Location = new System.Drawing.Point(98, 37);
+            this.rbModificar.Name = "rbModificar";
+            this.rbModificar.Size = new System.Drawing.Size(68, 17);
+            this.rbModificar.TabIndex = 1;
+            this.rbModificar.TabStop = true;
+            this.rbModificar.Text = "Modificar";
+            this.rbModificar.UseVisualStyleBackColor = true;
+            // 
+            // rbEliminar
+            // 
+            this.rbEliminar.AutoSize = true;
+            this.rbEliminar.Location = new System.Drawing.Point(197, 37);
+            this.rbEliminar.Name = "rbEliminar";
+            this.rbEliminar.Size = new System.Drawing.Size(61, 17);
+            this.rbEliminar.TabIndex = 2;
+            this.rbEliminar.TabStop = true;
+            this.rbEliminar.Text = "Eliminar";
+            this.rbEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(316, 34);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 3;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lstDatos);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(15, 167);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(410, 268);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lista de Paises";
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.Location = new System.Drawing.Point(3, 16);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(404, 249);
+            this.lstDatos.TabIndex = 0;
+            // 
+            // GestionPaises
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "GestionPaises";
+            this.Text = "GestionPaises";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbNuevo;
+        private System.Windows.Forms.RadioButton rbEliminar;
+        private System.Windows.Forms.RadioButton rbModificar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lstDatos;
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
