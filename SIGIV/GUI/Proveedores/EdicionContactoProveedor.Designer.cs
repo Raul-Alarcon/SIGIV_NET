17e80313739accd8ab04ed8a57e6206ae7089e38
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-06-05 10:57:43 -0600

    ....

--- SIGIV/GUI/Proveedores/EdicionContactoProveedor.Designer.cs
+++ SIGIV/GUI/Proveedores/EdicionContactoProveedor.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txbnombresContacto = new System.Windows.Forms.TextBox();
-            this.txbcargoContacto = new System.Windows.Forms.TextBox();
-            this.txbApellidosContacto = new System.Windows.Forms.TextBox();
-            this.txbtelefonoContacto = new System.Windows.Forms.TextBox();
-            this.txbobservacion = new System.Windows.Forms.TextBox();
-            this.txbeMailContacto = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.btnGuardar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbeMailContacto = new System.Windows.Forms.TextBox();
+            this.txbobservacion = new System.Windows.Forms.TextBox();
+            this.txbtelefonoContacto = new System.Windows.Forms.TextBox();
+            this.txbApellidosContacto = new System.Windows.Forms.TextBox();
+            this.txbcargoContacto = new System.Windows.Forms.TextBox();
+            this.txbnombresContacto = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -80,97 +80,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Formulario";
             // 
-            // txbnombresContacto
+            // btnGuardar
             // 
-            this.txbnombresContacto.Location = new System.Drawing.Point(40, 52);
-            this.txbnombresContacto.Name = "txbnombresContacto";
-            this.txbnombresContacto.Size = new System.Drawing.Size(306, 20);
-            this.txbnombresContacto.TabIndex = 0;
-            // 
-            // txbcargoContacto
-            // 
-            this.txbcargoContacto.Location = new System.Drawing.Point(40, 165);
-            this.txbcargoContacto.Name = "txbcargoContacto";
-            this.txbcargoContacto.Size = new System.Drawing.Size(150, 20);
-            this.txbcargoContacto.TabIndex = 1;
-            // 
-            // txbApellidosContacto
-            // 
-            this.txbApellidosContacto.Location = new System.Drawing.Point(40, 112);
-            this.txbApellidosContacto.Name = "txbApellidosContacto";
-            this.txbApellidosContacto.Size = new System.Drawing.Size(306, 20);
-            this.txbApellidosContacto.TabIndex = 2;
-            // 
-            // txbtelefonoContacto
-            // 
-            this.txbtelefonoContacto.Location = new System.Drawing.Point(196, 165);
-            this.txbtelefonoContacto.Name = "txbtelefonoContacto";
-            this.txbtelefonoContacto.Size = new System.Drawing.Size(150, 20);
-            this.txbtelefonoContacto.TabIndex = 3;
-            // 
-            // txbobservacion
-            // 
-            this.txbobservacion.Location = new System.Drawing.Point(40, 273);
-            this.txbobservacion.Multiline = true;
-            this.txbobservacion.Name = "txbobservacion";
-            this.txbobservacion.Size = new System.Drawing.Size(306, 100);
-            this.txbobservacion.TabIndex = 4;
-            // 
-            // txbeMailContacto
-            // 
-            this.txbeMailContacto.Location = new System.Drawing.Point(40, 219);
-            this.txbeMailContacto.Name = "txbeMailContacto";
-            this.txbeMailContacto.Size = new System.Drawing.Size(306, 20);
-            this.txbeMailContacto.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Nombres";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 96);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Apellidos";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 149);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Cargo";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(193, 149);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Telefono";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(37, 203);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Email";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnGuardar.Location = new System.Drawing.Point(40, 391);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
@@ -182,14 +99,97 @@
             this.label6.Text = "Comentario";
             this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // btnGuardar
+            // label5
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(40, 391);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
-            this.btnGuardar.TabIndex = 12;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Telefono";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cargo";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Apellidos";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombres";
+            // 
+            // txbeMailContacto
+            // 
+            this.txbeMailContacto.Location = new System.Drawing.Point(40, 219);
+            this.txbeMailContacto.Name = "txbeMailContacto";
+            this.txbeMailContacto.Size = new System.Drawing.Size(306, 20);
+            this.txbeMailContacto.TabIndex = 5;
+            // 
+            // txbobservacion
+            // 
+            this.txbobservacion.Location = new System.Drawing.Point(40, 273);
+            this.txbobservacion.Multiline = true;
+            this.txbobservacion.Name = "txbobservacion";
+            this.txbobservacion.Size = new System.Drawing.Size(306, 100);
+            this.txbobservacion.TabIndex = 4;
+            // 
+            // txbtelefonoContacto
+            // 
+            this.txbtelefonoContacto.Location = new System.Drawing.Point(196, 165);
+            this.txbtelefonoContacto.Name = "txbtelefonoContacto";
+            this.txbtelefonoContacto.Size = new System.Drawing.Size(150, 20);
+            this.txbtelefonoContacto.TabIndex = 3;
+            // 
+            // txbApellidosContacto
+            // 
+            this.txbApellidosContacto.Location = new System.Drawing.Point(40, 112);
+            this.txbApellidosContacto.Name = "txbApellidosContacto";
+            this.txbApellidosContacto.Size = new System.Drawing.Size(306, 20);
+            this.txbApellidosContacto.TabIndex = 2;
+            // 
+            // txbcargoContacto
+            // 
+            this.txbcargoContacto.Location = new System.Drawing.Point(40, 165);
+            this.txbcargoContacto.Name = "txbcargoContacto";
+            this.txbcargoContacto.Size = new System.Drawing.Size(150, 20);
+            this.txbcargoContacto.TabIndex = 1;
+            // 
+            // txbnombresContacto
+            // 
+            this.txbnombresContacto.Location = new System.Drawing.Point(40, 52);
+            this.txbnombresContacto.Name = "txbnombresContacto";
+            this.txbnombresContacto.Size = new System.Drawing.Size(306, 20);
+            this.txbnombresContacto.TabIndex = 0;
             // 
             // EdicionContactoProveedor
             // 
--- SIGIV/GUI/Proveedores/EdicionContactoProveedor.cs
+++ SIGIV/GUI/Proveedores/EdicionContactoProveedor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SIGIV.CLS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
