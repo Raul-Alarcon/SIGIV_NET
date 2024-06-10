5881d99f76edee6be9ac22c50cd1dcc58f127410
Raul-Alarcon <128653762+Raul-Alarcon@users.noreply.github.com>
2024-06-09 18:33:48 -0600

    correccion de eventos

--- SIGIV/DashBoard.Designer.cs
+++ SIGIV/DashBoard.Designer.cs
@@ -58,7 +58,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 129);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(951, 656);
+            this.panel1.Size = new System.Drawing.Size(983, 656);
             this.panel1.TabIndex = 13;
             // 
             // ContentLayout
@@ -66,7 +66,7 @@
             this.ContentLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ContentLayout.Location = new System.Drawing.Point(0, 0);
             this.ContentLayout.Name = "ContentLayout";
-            this.ContentLayout.Size = new System.Drawing.Size(951, 656);
+            this.ContentLayout.Size = new System.Drawing.Size(983, 656);
             this.ContentLayout.TabIndex = 11;
             // 
             // panel3
@@ -76,13 +76,13 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 30);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(951, 35);
+            this.panel3.Size = new System.Drawing.Size(983, 35);
             this.panel3.TabIndex = 11;
             // 
             // btnCerrarSession
             // 
             this.btnCerrarSession.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCerrarSession.Location = new System.Drawing.Point(814, 5);
+            this.btnCerrarSession.Location = new System.Drawing.Point(846, 5);
             this.btnCerrarSession.Name = "btnCerrarSession";
             this.btnCerrarSession.Size = new System.Drawing.Size(107, 23);
             this.btnCerrarSession.TabIndex = 1;
@@ -94,7 +94,7 @@
             // 
             this.lnlUser.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.lnlUser.AutoSize = true;
-            this.lnlUser.Location = new System.Drawing.Point(727, 10);
+            this.lnlUser.Location = new System.Drawing.Point(759, 10);
             this.lnlUser.Name = "lnlUser";
             this.lnlUser.Size = new System.Drawing.Size(35, 13);
             this.lnlUser.TabIndex = 0;
@@ -107,7 +107,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(3, 64);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(951, 65);
+            this.panel2.Size = new System.Drawing.Size(983, 65);
             this.panel2.TabIndex = 2;
             // 
             // panel4
@@ -116,7 +116,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(951, 30);
+            this.panel4.Size = new System.Drawing.Size(983, 30);
             this.panel4.TabIndex = 0;
             // 
             // materialTabControl1
@@ -137,7 +137,7 @@
             this.materialTabControl1.Multiline = true;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(951, 33);
+            this.materialTabControl1.Size = new System.Drawing.Size(983, 33);
             this.materialTabControl1.TabIndex = 0;
             this.materialTabControl1.SelectedIndexChanged += new System.EventHandler(this.materialTabControl1_SelectedIndexChanged_1);
             // 
@@ -146,7 +146,7 @@
             this.Home.Location = new System.Drawing.Point(4, 22);
             this.Home.Name = "Home";
             this.Home.Padding = new System.Windows.Forms.Padding(3);
-            this.Home.Size = new System.Drawing.Size(943, 7);
+            this.Home.Size = new System.Drawing.Size(975, 7);
             this.Home.TabIndex = 0;
             this.Home.Text = "Home";
             this.Home.UseVisualStyleBackColor = true;
@@ -156,7 +156,7 @@
             this.Empleados.Location = new System.Drawing.Point(4, 22);
             this.Empleados.Name = "Empleados";
             this.Empleados.Padding = new System.Windows.Forms.Padding(3);
-            this.Empleados.Size = new System.Drawing.Size(927, 7);
+            this.Empleados.Size = new System.Drawing.Size(959, 7);
             this.Empleados.TabIndex = 1;
             this.Empleados.Text = "Empleados";
             this.Empleados.UseVisualStyleBackColor = true;
@@ -165,7 +165,7 @@
             // 
             this.Usuarios.Location = new System.Drawing.Point(4, 22);
             this.Usuarios.Name = "Usuarios";
-            this.Usuarios.Size = new System.Drawing.Size(927, 7);
+            this.Usuarios.Size = new System.Drawing.Size(959, 7);
             this.Usuarios.TabIndex = 2;
             this.Usuarios.Text = "Usuarios";
             this.Usuarios.UseVisualStyleBackColor = true;
@@ -174,7 +174,7 @@
             // 
             this.Clientes.Location = new System.Drawing.Point(4, 22);
             this.Clientes.Name = "Clientes";
-            this.Clientes.Size = new System.Drawing.Size(927, 7);
+            this.Clientes.Size = new System.Drawing.Size(959, 7);
             this.Clientes.TabIndex = 3;
             this.Clientes.Text = "Clientes";
             this.Clientes.UseVisualStyleBackColor = true;
@@ -183,7 +183,7 @@
             // 
             this.Facturas.Location = new System.Drawing.Point(4, 22);
             this.Facturas.Name = "Facturas";
-            this.Facturas.Size = new System.Drawing.Size(927, 7);
+            this.Facturas.Size = new System.Drawing.Size(959, 7);
             this.Facturas.TabIndex = 4;
             this.Facturas.Text = "Facturas";
             this.Facturas.UseVisualStyleBackColor = true;
@@ -192,7 +192,7 @@
             // 
             this.Proveedores.Location = new System.Drawing.Point(4, 22);
             this.Proveedores.Name = "Proveedores";
-            this.Proveedores.Size = new System.Drawing.Size(927, 7);
+            this.Proveedores.Size = new System.Drawing.Size(959, 7);
             this.Proveedores.TabIndex = 5;
             this.Proveedores.Text = "Proveedores";
             this.Proveedores.UseVisualStyleBackColor = true;
@@ -201,7 +201,7 @@
             // 
             this.Pedidos.Location = new System.Drawing.Point(4, 22);
             this.Pedidos.Name = "Pedidos";
-            this.Pedidos.Size = new System.Drawing.Size(927, 7);
+            this.Pedidos.Size = new System.Drawing.Size(959, 7);
             this.Pedidos.TabIndex = 6;
             this.Pedidos.Text = "Pedidos";
             this.Pedidos.UseVisualStyleBackColor = true;
@@ -210,7 +210,7 @@
             // 
             this.Productos.Location = new System.Drawing.Point(4, 22);
             this.Productos.Name = "Productos";
-            this.Productos.Size = new System.Drawing.Size(927, 7);
+            this.Productos.Size = new System.Drawing.Size(959, 7);
             this.Productos.TabIndex = 7;
             this.Productos.Text = "Productos";
             this.Productos.UseVisualStyleBackColor = true;
@@ -219,7 +219,7 @@
             // 
             this.Reportes.Location = new System.Drawing.Point(4, 22);
             this.Reportes.Name = "Reportes";
-            this.Reportes.Size = new System.Drawing.Size(927, 7);
+            this.Reportes.Size = new System.Drawing.Size(959, 7);
             this.Reportes.TabIndex = 8;
             this.Reportes.Text = "Reportes";
             this.Reportes.UseVisualStyleBackColor = true;
@@ -228,7 +228,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(957, 788);
+            this.ClientSize = new System.Drawing.Size(989, 788);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.DrawerAutoHide = false;
